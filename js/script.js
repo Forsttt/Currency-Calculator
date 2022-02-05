ca002dce3b9b6e522bb898661d2cf5bf9bc87c00
Szymon Forst <szymonforstbiznes@gmail.com>
2022-02-05 18:39:10 +0100

    JS clean up

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-let euroElement = document.querySelector(".js-euro");
-let form = document.querySelector(".form");
-let rateElement = document.querySelector(".js-rate");
-let outputElement = document.querySelector(".js-output");
-let button = document.querySelector(".button");
+{
+    const form = document.querySelector(".js-form");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let euro = euroElement.value;
-    let rate = rateElement.value;
+    form.addEventListener("submit", (event) => {
+        const euroElement = document.querySelector(".js-euro");
+        const rateElement = document.querySelector(".js-rate");
+        const outputElement = document.querySelector(".js-output");
+        event.preventDefault();
+        
+        let euro = euroElement.value;
+        let rate = rateElement.value;
 
-    let output = euro * rate;
-    outputElement.innerText = output.toFixed(2) + "PLN";
-    console.log(output);
-});
+        let output = euro * rate;
+        outputElement.innerText = output.toFixed(2) + "PLN";
+        console.log(output);
+    });
+
+}
