ebd31278d0beefb7f8da539b019043628fe1d07e
Szymon Forst <szymonforstbiznes@gmail.com>
2022-02-06 18:58:13 +0100

    changed to select

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
 {
-    const form = document.querySelector(".js-form");
+const welcome = () => {
+  console.log("witam :)");
+};
 
-    form.addEventListener("submit", (event) => {
-        const euroElement = document.querySelector(".js-euro");
-        const rateElement = document.querySelector(".js-rate");
-        const outputElement = document.querySelector(".js-output");
-        event.preventDefault();
-        
-        let euro = euroElement.value;
-        let rate = rateElement.value;
+const calculateResult = (amount, currency) => {
+  const rateEUR = 4.5817;
+  const rateCHF = 4.3392;
+  const rateSEK = 0.4382;
 
-        let output = euro * rate;
-        outputElement.innerText = output.toFixed(2) + "PLN";
-        console.log(output);
-    });
+  switch(currency) {
+    case "EUR":
+      return amount / rateEUR;
+      
+      case "CHF":
+        return amount / rateCHF;
+
+        case "SEK":
+          return amount / rateSEK;
+  }
+};
+
+const SubmitResponse = (event) => {
+  event.preventDefault();
+
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+  const resultElement = document.querySelector(".js-result");
+
+  const amount = amountElement.value;
+  const currency = currencyElement.value;
+
+  let result = calculateResult(amount, currency)
+
+  resultElement.innerText = `${result.toFixed(2)} ${currency}`;
+};
+
+const init = () => {
+  const formElement = document.querySelector(".js-form");
+
+  formElement.addEventListener("submit", SubmitResponse);
+};
+
+welcome();
+init();
 
 }
