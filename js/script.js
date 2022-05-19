fef26127bf70d040c6ec7d0ee8e847bbbe15ed4c
Szymon Forst <szymonforstbiznes@gmail.com>
2022-05-19 11:17:34 +0200

    fixed site responsiveness

--- js/script.js
+++ js/script.js
@@ -1,49 +1,47 @@
 {
-const welcome = () => {
-  console.log("witam :)");
-};
+  const welcome = () => {
+    console.log("witam :)");
+  };
 
-const calculateResult = (amount, currency) => {
-  const rateEUR = 4.5817;
-  const rateCHF = 4.3392;
-  const rateSEK = 0.4382;
+  const calculateResult = (amount, currency) => {
+    const rateEUR = 4.5817;
+    const rateCHF = 4.3392;
+    const rateSEK = 0.4382;
 
-  switch(currency) {
-    case "EUR":
-      return amount / rateEUR;
-      
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
+
       case "CHF":
         return amount / rateCHF;
 
-        case "SEK":
-          return amount / rateSEK;
-  }
-};
+      case "SEK":
+        return amount / rateSEK;
+    }
+  };
 
-const SubmitResponse = (event) => {
-  event.preventDefault();
+  const submitResponse = (event) => {
+    event.preventDefault();
 
-  const amountElement = document.querySelector(".js-amount");
-  const currencyElement = document.querySelector(".js-currency");
-  const resultElement = document.querySelector(".js-result");
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
 
-  const amount = amountElement.value;
-  const currency = currencyElement.value;
+    const amount = amountElement.value;
+    const currency = currencyElement.value;
 
-  let result = calculateResult(amount, currency)
+    let result = calculateResult(amount, currency)
+    resultElement.innerText = `${result.toFixed(2)} ${currency}`;
+  };
 
-  resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-};
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", submitResponse);
 
-const init = () => {
-  const formElement = document.querySelector(".js-form");
-
-  formElement.addEventListener("submit", SubmitResponse);
-
-  welcome();
-};
+    welcome();
+  };
 
 
-init();
+  init();
 
 }
